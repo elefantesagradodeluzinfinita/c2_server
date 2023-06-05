f9bbad59eb68988b2ca46c9e1b963cf6a3fac2eb
Mayteelsoon Guerra Castilla <elefantesagradodeluzinfinita@gmail.com>
2023-06-04 22:53:50 -0500

    socket for microphone streaming

--- server.py
+++ server.py
@@ -1,72 +1,25 @@
-import logging
+import socket
 import pyaudio
-import wave
-import requests
-import api
-from flask import Flask, request, Response, stream_with_context
-import threading
 
-app = Flask(__name__)
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Ruta principal
-@app.route("/")
-def main():
-    logging.info("Se ejecutó la función 'main'")
-    return api.get_main()
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 10000)
+print('connecting to %s port %s' % server_address)
+sock.connect(server_address)
 
-@app.route('/stream_audio')
-def stream_audio():
-    CHUNK_SIZE = 1024
-    RATE = 44100
+# Set up pyaudio
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
 
-    p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16,
-                    channels=1,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK_SIZE)
+# Stream audio from microphone to socket
+while True:
+    data = stream.read(1024)
+    sock.sendall(data)
 
-    def generate():
-        while True:
-            data = stream.read(CHUNK_SIZE)
-            yield data
-
-    return Response(generate(), mimetype='audio/x-wav')
-
-# Ruta para reproducir el archivo "attack.wav"
-@app.route("/play_attack", methods=['GET'])
-def play_attack():
-    logging.info("Reproduciendo el archivo 'attack.wav'")
-    threading.Thread(target=play_audio).start()
-    return "Reproduciendo 'attack.wav'"
-
-def play_audio():
-    # Configuración del archivo de audio
-    audio_file = "attack.wav"
-    chunk = 1024
-
-    # Inicializar PyAudio
-    p = pyaudio.PyAudio()
-
-    # Abrir el archivo de audio
-    wf = wave.open(audio_file, 'rb')
-
-    # Abrir el stream de reproducción
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    output=True)
-
-    # Leer y reproducir el audio en bloques
-    data = wf.readframes(chunk)
-    while data != b'':
-        stream.write(data)
-        data = wf.readframes(chunk)
-
-    # Cerrar los streams y terminar PyAudio
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(9000))
+# Clean up
+stream.stop_stream()
+stream.close()
+p.terminate()
+sock.close()
